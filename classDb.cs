5db6ea38d06bc405c39e34a162cc3bb751384654
chanisiroka <60046354+chanisiroka@users.noreply.github.com>
2024-01-29 00:22:39 +0200

    Add files via upload

---
+++ classDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal;
+using Dto;
+namespace Bll
+{
+   public class classDb
+    {
+        
+        public RequestResult GetStudents()
+        {
+           // try
+           // {
+                using (databaseOrEntities db = new databaseOrEntities())
+                {
+                    List<studentTblDto> lst = new List<studentTblDto>();
+                    foreach (var item in db.studentTbl.ToList())
+                    {
+                        lst.Add(studentTblDto.DalToDto(item));
+                    }
+                    return new RequestResult { Data = lst, Status = true, Message = "" };
+                }
+          //  }
+            //catch (Exception ex)
+            //{
+            //    return new RequestResult { Data = null, Status = false, Message = "שגיאה בשליפת הנתונים" };
+            //}
+        }
+    }
+}
---
+++ studentTblDto.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto
+{
+    [Serializable]
+   public class studentTblDto
+    {
+        public int idStudent { get; set; }
+        public string tzStudent { get; set; }
+        public string lastName { get; set; }
+        public string firstname { get; set; }
+        public string connect { get; set; }
+        public string kita { get; set; }
+        public string madricha { get; set; }
+        public string teacher { get; set; }
+        public string @class { get; set; }
+        public string miun { get; set; }
+        public string phone { get; set; }
+        public Nullable<int> yaad { get; set; }
+        public Nullable<int> untill { get; set; }
+
+        public studentTbl DtoToDal() {
+
+            var config = new MapperConfiguration(cfg =>
+                     cfg.CreateMap<studentTblDto, studentTbl>()
+                 );
+            var mapper = new Mapper(config);
+            return mapper.Map<studentTbl>(this);
+
+        }
+        public static studentTblDto DalToDto(studentTbl student)
+        {
+
+            var config = new MapperConfiguration(cfg =>
+                     cfg.CreateMap<studentTbl, studentTblDto>()
+                 );
+            var mapper = new Mapper(config);
+            return mapper.Map<studentTblDto>(student);
+
+        }
+    }
+}
